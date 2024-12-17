cd85917e71e00dd0a64ae42a6409c9f6a390aec5
mahabur-rahman <annur4395@gmail.com>
2024-12-17 18:15:32 +0600

    Set up project for main with microservice

--- main/src/main.ts
+++ main/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 7000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
---
+++ main/src/product/product.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './product.service';
+import { Product } from './schema/product.schema';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async create(@Body() productDto: any): Promise<Product> {
+    return this.productsService.create(productDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Product> {
+    return this.productsService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() productDto: any,
+  ): Promise<Product> {
+    return this.productsService.update(id, productDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Product> {
+    return this.productsService.delete(id);
+  }
+}
